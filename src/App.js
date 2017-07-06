a7d3408e0ac97d8ba62bfe9aaea58c450ceaee5b
Daryl Cole <darylcoleoffice@gmail.com>
2017-07-06 22:46:42 +0900

    aded routing to front page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Route } from 'react-router-dom'
 import SearchBooks from './SearchBooks'
 import OpenSearch from './OpenSearch'
 import Shelf from './Shelf'
-import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
@@ -11,31 +10,33 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
+        <Route exact path='/' render={() => (
           <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
             </div>
-            <div className="list-books-content">
-              <div>
-                <Shelf
-                shelfName='Currently Reading'
-                shelfType='currentlyReading'
-                />
-                <Shelf
-                shelfName='Want To Read'
-                shelfType='wantToRead'
-                />
-                <Shelf
-                shelfName='Read'
-                shelfType='read'
-                />
+              <div className="list-books-content">
+                <div>
+                  <Shelf
+                  shelfName='Currently Reading'
+                  shelfType='currentlyReading'
+                  />
+                  <Shelf
+                  shelfName='Want To Read'
+                  shelfType='wantToRead'
+                  />
+                  <Shelf
+                  shelfName='Read'
+                  shelfType='read'
+                  />
               </div>
-              <OpenSearch/>
-              <Route path='/add' render={() => (
-                <SearchBooks/>
-              )}/>
             </div>
-        </div>
+          </div>
+        )}/>
+        <OpenSearch/>
+        <Route path='/add' render={() => (
+          <SearchBooks/>
+        )}/>
       </div>
       )
     }
