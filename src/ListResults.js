9a9eb8ddc97d61433d3a5feac728c9fa0f4752dc
daryl <darylcoleoffice@gmail.com>
2017-07-28 11:04:56 +0900

    linting ListResults.js

--- src/ListResults.js
+++ src/ListResults.js
@@ -1,34 +1,36 @@
 import React, { Component } from 'react';
-import Book from './Book'
+import Book from './Book';
 
 class ListResults extends Component {
-
   render() {
-    const { searchResults, updateBookStatus, setBookUrl, addBookToMove } = this.props
+    const { searchResults, updateBookStatus, setBookUrl, addBookToMove } = this.props;
+
     return (
-        <div className="list-results">
-          <h2 className="results-title">Search Results</h2>
-          <div className="found-books">
-            <ol className='books-grid'>
-              {searchResults.map((book) => (
-                <li key={book.id} className='book-item'>
-                  <Book
+      <div className="list-results">
+        <h2 className="results-title">Search Results</h2>
+        <div className="found-books">
+          <ol className="books-grid">
+            {searchResults.map(book => (
+              <li key={book.id} className="book-item">
+                <Book
                   book={book}
                   onUpdateShelf={(book, shelfType) => {
-                    updateBookStatus(book, shelfType)
+                    updateBookStatus(book, shelfType);
                   }}
-                  setBookUrl={(newUrl, book) => {setBookUrl(newUrl, book)
+                  setBookUrl={(newUrl, book) => {
+                    setBookUrl(newUrl, book);
                   }}
-                  addBookToMove={(bookToMove, value) => {addBookToMove(bookToMove, value)
+                  addBookToMove={(bookToMove, value) => {
+                    addBookToMove(bookToMove, value);
                   }}
-                  />
-                </li>
-              ))}
-            </ol>
+                />
+              </li>
+            ))}
+          </ol>
         </div>
-       </div>
-    )
+      </div>
+    );
   }
 }
 
-export default ListResults
+export default ListResults;
