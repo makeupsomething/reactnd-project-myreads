a590417170313424f734b6682d5768fe19af790a
daryl <darylcoleoffice@gmail.com>
2017-07-28 11:09:00 +0900

    linting ListShelves.js

--- src/ListShelves.js
+++ src/ListShelves.js
@@ -1,27 +1,26 @@
 import React, { Component } from 'react';
-import Shelf from './Shelf'
+import Shelf from './Shelf';
 
 class ListShelves extends Component {
-
   render() {
-    const { books, updateBookStatus, setBookUrl, addBookToMove } = this.props
+    const { books, updateBookStatus, setBookUrl, addBookToMove } = this.props;
 
-    var shelfTypes = ['Currently Reading', 'Want To Read', 'Read']
+    var shelfTypes = ['Currently Reading', 'Want To Read', 'Read'];
 
     return (
-        <div className="list-books-content">
-          <div>
-            {shelfTypes.map(item => <Shelf
+      <div className="list-books-content">
+        <div>
+          {shelfTypes.map(item => (<Shelf
             shelfName={item}
             books={books}
-            updateBookStatus={(book, shelfType) => {updateBookStatus(book, shelfType)}}
-            setBookUrl={(newUrl, book) => {setBookUrl(newUrl, book)}}
-            addBookToMove={(bookToMove, value) => {addBookToMove(bookToMove, value)}}
-            />)}
+            updateBookStatus={(book, shelfType) => { updateBookStatus(book, shelfType); }}
+            setBookUrl={(newUrl, book) => { setBookUrl(newUrl, book); }}
+            addBookToMove={(bookToMove, value) => { addBookToMove(bookToMove, value); }}
+          />))}
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default ListShelves
+export default ListShelves;
