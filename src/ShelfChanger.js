8c02c24c489782d90982f57904338fc96a653f04
daryl <darylcoleoffice@gmail.com>
2017-07-28 11:21:24 +0900

    lintnig ShelfChanger.js

--- src/ShelfChanger.js
+++ src/ShelfChanger.js
@@ -7,17 +7,17 @@ class ShelfChanger extends Component {
     return (
       <div className="book-shelf-changer">
         <form>
-            <select value={book.shelf} onChange={handleChange}>
-              <option value="none" disabled>Move to...</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="none">None</option>
-            </select>
+          <select value={book.shelf} onChange={handleChange}>
+            <option value="none" disabled>Move to...</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="none">None</option>
+          </select>
         </form>
       </div>
-    )
+    );
   }
 }
 
-export default ShelfChanger
+export default ShelfChanger;
