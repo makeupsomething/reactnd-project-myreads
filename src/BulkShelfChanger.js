817c5ac666b40e6d91c79dee10860da7bd00afe7
daryl <darylcoleoffice@gmail.com>
2017-07-28 10:57:47 +0900

    linting BulkShelfChanger.js

--- src/BulkShelfChanger.js
+++ src/BulkShelfChanger.js
@@ -1,31 +1,30 @@
 import React, { Component } from 'react';
 
 class BulkShelfChanger extends Component {
-
   constructor(props) {
-      super(props);
-      this.handleChange = this.handleChange.bind(this);
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(event) {
-    this.props.bulkUpdateBookStatus(event.target.value)
+    this.props.bulkUpdateBookStatus(event.target.value);
   }
 
   render() {
     return (
       <div className="book-shelf-changer-bulk">
         <form>
-            <select value="none" onChange={this.handleChange}>
-              <option value="none" disabled>Move to...</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="none">None</option>
-            </select>
+          <select value="none" onChange={this.handleChange}>
+            <option value="none" disabled>Move to...</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="none">None</option>
+          </select>
         </form>
       </div>
-    )
+    );
   }
 }
 
-export default BulkShelfChanger
+export default BulkShelfChanger;
