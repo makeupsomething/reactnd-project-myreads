172799f6289946140d7b70eab592b681c5793c2f
daryl <darylcoleoffice@gmail.com>
2017-07-31 16:06:51 +0900

    added comments to OpenSearch.js

--- src/OpenSearch.js
+++ src/OpenSearch.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+/**
+* @description Button for opening the search page
+*/
 class OpenSearch extends Component {
+  /**
+  * @description The render function
+  * @returns { object } The UI
+  */
   render() {
     return (
       <div className="open-search">
